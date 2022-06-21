11433c215ec0cb9c006650d23e8894344819f0ef
Ryan Shane Palm <rspalm@bigpond.com>
2022-06-21 20:03:58 +0200

    Started work on loading services

--- src/Application.php
+++ src/Application.php
@@ -2,17 +2,21 @@
 
 namespace Palmyr\Console;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Application AS BaseApplication;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 abstract class Application extends BaseApplication
 {
 
-    private string $rootDirectory;
+    private string $projectDirectory;
 
-    protected function __construct()
+    private string $consoleDirectory;
+
+    protected function __construct(string $name = 'app', string $version = '1.0.0')
     {
-        parent::__construct('git-hook', '1.0.0');
+        parent::__construct($name, $version);
     }
 
     final static public function init(): void
@@ -23,26 +27,51 @@ abstract class Application extends BaseApplication
 
         $containerBuilder->set('container', $containerBuilder);
         $containerBuilder->set('application', $application);
-        $containerBuilder->setParameter('root_directory', $application->getRootDirectory());
+        $containerBuilder->setParameter('project_directory', $application->getprojectDirectory());
+        $containerBuilder->setParameter('console_directory', $application->getConsoleDirectory());
 
-        $application->boot();
+        $application->boot(
+            new YamlFileLoader($containerBuilder, new FileLocator())
+        );
 
         $containerBuilder->compile(true);
 
         $application->run();
     }
 
-    final protected function getRootDirectory(): string
+    protected function getProjectDirectory(): string
     {
-        if ( !isset($this->rootDirectory) ) {
-            $this->rootDirectory = __DIR__;
+        if ( !isset($this->projectDirectory) ) {
+            $r = new \ReflectionObject($this);
+
+            if (!is_file($dir = $r->getFileName())) {
+                throw new \LogicException(sprintf('Cannot auto-detect project dir for kernel of class "%s".', $r->name));
+            }
+
+            $dir = $rootDir = \dirname($dir);
+            while (!is_file($dir.'/composer.json')) {
+                if ($dir === \dirname($dir)) {
+                    return $this->projectDirectory = $rootDir;
+                }
+                $dir = \dirname($dir);
+            }
+            $this->projectDirectory = $dir;
         }
-        return $this->rootDirectory;
+        return $this->projectDirectory;
     }
 
-    protected function boot(): void
+    private function getConsoleDirectory(): string
     {
+        if ( !isset($this->consoleDirectory) ) {
+            $this->consoleDirectory = dirname(__DIR__);
+        }
+        return $this->consoleDirectory;
+    }
 
+    protected function boot(YamlFileLoader $loader): void
+    {
+        $loader->load($this->getProjectDirectory() . '/config/services.yaml');
+        $loader->load($this->getConsoleDirectory() . '/config/services.yaml');
     }
 
 }
